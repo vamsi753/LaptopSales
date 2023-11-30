571fb57a4bb24fa7f90eb1c2d3b5d09f4f44ca61
@vamsi753 <ltybjy666@gmail.com>
2023-11-30 13:06:23 +0530

    first commit

---
+++ LaptopSales/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LaptopSales.Startup))]
+namespace LaptopSales
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
